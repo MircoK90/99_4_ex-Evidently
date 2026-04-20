8d43ee737f78332b044683dbb74f6e5c8afb2f86
Mirco <mirco.kappel@gmx.de>
2026-04-20 10:34:37 +0200

    update

--- examples.py
+++ examples.py
@@ -6,6 +6,7 @@ import requests
 import zipfile
 import io
 import json
+import pathlib as path
 
 from sklearn import datasets, ensemble, model_selection
 from scipy.stats import anderson_ksamp
@@ -19,14 +20,12 @@ import warnings
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
 
-
-
-url = "https://archive.ics.uci.edu/static/public/275/bike+sharing+dataset.zip"
+URL = "https://archive.ics.uci.edu/static/public/275/bike+sharing+dataset.zip"
 
 
 # custom functions
 def _fetch_data() -> pd.DataFrame:
-    content = requests.get(url, verify=False).content
+    content = requests.get(URL, verify=False).content
 
     with zipfile.ZipFile(io.BytesIO(content)) as arc:
         raw_data = pd.read_csv(arc.open("hour.csv"), header=0, sep=',', parse_dates=['dteday']) 
@@ -37,11 +36,10 @@ def _process_data(raw_data: pd.DataFrame) -> pd.DataFrame:
     return raw_data
 
 
+
 # load data
 raw_data = _process_data(_fetch_data())
 print(raw_data.head())
-
-
 
 # Feature selection
 target = 'cnt'
@@ -49,24 +47,32 @@ prediction = 'prediction'
 numerical_features = ['temp', 'atemp', 'hum', 'windspeed', 'mnth', 'hr', 'weekday']
 categorical_features = ['season', 'holiday', 'workingday']
 
+
 # like the purposel of the exam
-def column_mapping():
+def column_mapping(
+        target: str,
+        prediction: str,
+        numerical_features: list[str],
+        categorical_features: list[str]
+):
     column_mapping = ColumnMapping()
-    column_mapping.target = 'cnt',
-    column_mapping.numerical_features = numerical_features
-    column_mapping.categorical_features = categorical_features
+    column_mapping.target = target,
+    column_mapping.prediction = prediction,
+    column_mapping.numerical_features = numerical_features,
+    column_mapping.categorical_features = categorical_features,
     return column_mapping
 
 
+REPORTS_DIR = path("reports")
+REPORTS_DIR.mkdir(parents=True, exist_ok=True)
 
 
-
-# Reference and current data splitpip show evidently
-reference_jan11 = raw_data.loc['2011-01-01 00:00:00':'2011-01-28 23:00:00']
-current_feb11 = raw_data.loc['2011-01-29 00:00:00':'2011-02-28 23:00:00']
 
 
 def main():
+    # Reference and current data splitpip show evidently
+    reference_jan11 = raw_data.loc['2011-01-01 00:00:00':'2011-01-28 23:00:00']
+    current_feb11 = raw_data.loc['2011-01-29 00:00:00':'2011-02-28 23:00:00']
 
     # Train test split ONLY on reference_jan11
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
@@ -87,7 +93,7 @@ def main():
 
     # Add actual target and prediction columns to the training data for later performance analysis
     # mk gets shaped back with y component and adds preds with another Column"
-    X_train['target'] = y_train
+    X_train['target'] = y_train    # in former file als 'cnt
     X_train['prediction'] = preds_train
 
     # Add actual target and prediction columns to the test data for later performance analysis
@@ -98,12 +104,12 @@ def main():
     column_mapping = ColumnMapping()
 
     # # Map the actual target and prediction column names in the dataset for evidently
-    column_mapping.target = 'target'
-    column_mapping.prediction = 'prediction'
+    # column_mapping.target = 'target'
+    # column_mapping.prediction = 'prediction'
 
     # # Specify which features are numerical and which are categorical for the evidently report
-    column_mapping.numerical_features = numerical_features
-    column_mapping.categorical_features = categorical_features
+    # column_mapping.numerical_features = numerical_features
+    # column_mapping.categorical_features = categorical_features
 
     # Initialize the regression performance report with the default regression metrics preset
     regression_performance_report = Report(metrics=[
@@ -120,12 +126,12 @@ def main():
     # Train the production model
     regressor.fit(reference_jan11[numerical_features + categorical_features], reference_jan11[target])
 
-    # Perform column mapping
-    column_mapping = ColumnMapping()
-    column_mapping.target = target
-    column_mapping.prediction = prediction
-    column_mapping.numerical_features = numerical_features
-    column_mapping.categorical_features = categorical_features
+    # # Perform column mapping
+    # column_mapping = ColumnMapping()
+    # column_mapping.target = target
+    # column_mapping.prediction = prediction
+    # column_mapping.numerical_features = numerical_features
+    # column_mapping.categorical_features = categorical_features
 
     # Generate predictions for the reference data
     ref_prediction = regressor.predict(reference_jan11[numerical_features + categorical_features])
@@ -140,24 +146,19 @@ def main():
     regression_performance_report.run(reference_data=None, 
                                     current_data=reference_jan11,
                                     column_mapping=column_mapping)
+    
 
-
-    column_mapping_drift = ColumnMapping()
-
-    column_mapping_drift.target = target
-    column_mapping_drift.prediction = prediction
-    column_mapping_drift.numerical_features = numerical_features
-    column_mapping_drift.categorical_features = []
 
     data_drift_report = Report(metrics=[
         DataDriftPreset(),
     ])
 
-    # data_drift_report.run(
-    #     reference_data=reference_jan11,
-    #     current_data=current_feb11.loc['2011-02-14 00:00:00':'2011-02-21 23:00:00'],
-    #     column_mapping=column_mapping_drift,
-    # )
+    data_drift_report.run(
+        reference_data=reference_jan11,
+        current_data=current_feb11.loc['2011-02-14 00:00:00':'2011-02-21 23:00:00'],
+        column_mapping=column_mapping_drift,
+    )
+    save_report(prod_reg_report, "02_production_january")
 
 if __name__ == "__main__":
     main()
